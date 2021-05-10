87e9697f9a7f6b7677fc0fbd98c9d47194b2dea9
Thomas Cournil <cournilthomas@gmail.com>
2021-05-10 17:41:38 +0200

    test

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -3,6 +3,7 @@ package test;
 import java.util.ArrayList;
 import java.util.List;
 
+import metier.Client;
 import metier.Console;
 import metier.Jeu;
 
@@ -16,8 +17,15 @@ public class Test {
 		Jeu Mario = new Jeu("Super Mario Bros. 3", consoles);
 		Jeu Zelda = new Jeu("Zelda A Link to the Past", consoles);
 		Jeu MarioKart = new Jeu("Mario Kart", consoles);
+		List<Jeu> jeux=new ArrayList<Jeu>();
+		jeux.add(Zelda);
+		jeux.add(MarioKart);
+		jeux.add(Mario);
 		Jeu F0 = new Jeu ("F-Zero", consoles);
 		Jeu DD = new Jeu("Double Dragon", consoles);
+		
+		Client Alphonse=new Client("Alphonse","Brown",jeux);
+		
 	}
 
 }
