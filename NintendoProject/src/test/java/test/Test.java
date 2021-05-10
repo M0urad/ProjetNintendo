d7a2a09fdb2cdbf1b40fe23b73b27877708e6313
MouradZ <mourad.zohri@gmail.com>
2021-05-10 16:42:19 +0200

    Creation jeux et console

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -1,10 +1,23 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import metier.Console;
+import metier.Jeu;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Console SNES = new Console("Super Famicom");
+		List<Console> consoles = new ArrayList<Console>();
+		consoles.add(SNES);
+		Jeu Mario = new Jeu("Super Mario Bros. 3", consoles);
+		Jeu Zelda = new Jeu("Zelda A Link to the Past", consoles);
+		Jeu MarioKart = new Jeu("Mario Kart", consoles);
+		Jeu F0 = new Jeu ("F-Zero", consoles);
+		Jeu DD = new Jeu("Double Dragon", consoles);
 	}
 
 }
